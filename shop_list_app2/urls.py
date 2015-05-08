249b8e80ad81299aa0d8be24315ba0b889a4f3ca
rbenvos <rben.vos@gmail.com>
2015-05-08 12:02:48 +0200

    v2.3.a Primera version del admin frontend. Permite la navegacion entre los diferentes modelos.

--- shop_list_app2/urls.py
+++ shop_list_app2/urls.py
@@ -5,8 +5,20 @@ import views
 urlpatterns = patterns('',
 
     url(r'^$', views.dashboard, name='dashboard'),
-    url(r'^groups/$', views.group_list, name='group_list'),
-    url(r'^groups/(?P<pk>[0-9]+)$', views.group_detail, name='group_detail'),
+
+    #Menu
+    url(r'^users/$', views.users_list_view, name='users_list_view'),
+    url(r'^groups/$', views.groups_list_view, name='groups_list_view'),
+    url(r'^devices/$', views.devices_list_view, name='devices_list_view'),
+    url(r'^orders/$', views.orders_list_view, name='orders_list_view'),
+    url(r'^products/$', views.products_list_view, name='products_list_view'),
+
+    #Detalle
+    url(r'^users/(?P<pk>[0-9]+)$', views.user_detail_view, name='user_detail_view'),
+    url(r'^groups/(?P<pk>[0-9]+)$', views.group_detail_view, name='group_detail_view'),
+    url(r'^devices/(?P<pk>[0-9]+)$', views.device_detail_view, name='device_detail_view'),
+    url(r'^orders/(?P<pk>[0-9]+)$', views.order_detail_view, name='order_detail_view'),
+    url(r'^products/(?P<pk>[0-9]+)$', views.product_detail_view, name='product_detail_view'),
 
     # URLS del API REST
     url(r'^api/1.0/users/$', UserListAPI.as_view(), name='api_user_list'),
--- shop_list_app2/views.py
+++ shop_list_app2/views.py
@@ -2,21 +2,60 @@ from django.shortcuts import render
 
 from models import User, Group,Product, Order, Phone
 
-# Create your views here.
+# Dashboard
 
 def dashboard(request):
-    db_user_list = User.objects.all()
-    db_group_list = Group.objects.all()
-    context = {'user_list': db_user_list,
-               'group_list': db_group_list}
+    db_users_list = User.objects.all()[:5]
+    db_groups_list = Group.objects.all()[:5]
+    db_devices_list = Phone.objects.all()[:5]
+    db_orders_list = Order.objects.all()[:5]
+    db_products_list = Product.objects.all()[:5]
+    context = {'db_users_list': db_users_list,
+               'db_groups_list': db_groups_list,
+               'db_devices_list': db_devices_list,
+               'db_orders_list': db_orders_list,
+               'db_products_list': db_products_list,
+               }
     return render(request, 'dashboard.html',context)
 
-def group_list(request):
-    group_list = Group.objects.all()
-    context = {'group_list': group_list}
-    return render(request, 'group_list.html',context)
 
-def group_detail(request, pk):
+#Main
+def groups_list_view(request):
+    groups_list = Group.objects.all()
+    context = {'groups_list': groups_list}
+    return render(request, 'groups_list.html',context)
+
+def users_list_view(request):
+    users_list = User.objects.all()
+    context = {'users_list': users_list}
+    return render(request, 'users_list.html',context)
+
+def devices_list_view(request):
+    devices_list = Phone.objects.all()
+    context = {'devices_list': devices_list}
+    return render(request, 'devices_list.html',context)
+
+def orders_list_view(request):
+    orders_list = Order.objects.all()
+    context = {'orders_list': orders_list}
+    return render(request, 'orders_list.html',context)
+
+def products_list_view(request):
+    products_list = Product.objects.all()
+    context = {'products_list': products_list}
+    return render(request, 'products_list.html',context)
+
+
+#Details
+def user_detail_view(request, pk):
+    '''
+    Presenta el detalle de una imagen
+    '''
+    user = User.objects.filter(pk=pk)[0]
+    context = { 'user_detail' : user }
+    return render(request, 'user_detail.html', context)
+
+def group_detail_view(request, pk):
     '''
     Presenta el detalle de una imagen
     '''
@@ -25,3 +64,27 @@ def group_detail(request, pk):
 
     context = { 'group_detail' : selected_group }
     return render(request, 'group_detail.html', context)
+
+def device_detail_view(request, pk):
+    '''
+    Presenta el detalle de una imagen
+    '''
+    device = Phone.objects.filter(pk=pk)[0]
+    context = { 'device_detail' : device }
+    return render(request, 'device_detail.html', context)
+
+def order_detail_view(request, pk):
+    '''
+    Presenta el detalle de una imagen
+    '''
+    order = Order.objects.filter(pk=pk)[0]
+    context = { 'order_detail' : order }
+    return render(request, 'order_detail.html', context)
+
+def product_detail_view(request, pk):
+    '''
+    Presenta el detalle de una imagen
+    '''
+    product = Product.objects.filter(pk=pk)[0]
+    context = { 'product_detail' : product }
+    return render(request, 'product_detail.html', context)
